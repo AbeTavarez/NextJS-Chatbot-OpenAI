52218818f87b85c0733cbcc5b81c942eb48e6729
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-13 22:50:00 -0400

    home page style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/ui/header";
+import Footer from "@/ui/footer";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,67 @@
 import Chatbot from "@/components/Chatbot/chatbot";
 
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>Chatbot with OpenAI</h1>
+      <section className="mb-40 text-center">
+        <h1 className="text-5xl mb-10 text-blue-400">
+          AI-Powered Chatbot: A Next-Gen Virtual Assistant
+        </h1>
+
+        <p className="text-xl">
+          Welcome to my AI-powered chatbot project, designed to deliver
+          intelligent, real-time assistance across various domains. Built with a
+          powerful stack—Next.js, TypeScript, Tailwind CSS, and OpenAI&apos;s
+          API—this chatbot showcases my proficiency in developing cutting-edge,
+          AI-driven applications.
+        </p>
+      </section>
+
+      <section className="mb-20 w-full">
+        <h2 className="text-4xl mb-10 text-blue-400">Core Features</h2>
+
+        <ul className="text-xl list-disc">
+          <li>
+            Real-Time AI Responses: Leveraging OpenAI to deliver smart,
+            context-aware responses.
+          </li>
+          <li>
+            Scalable Architecture: Built with Next.js for optimal performance
+            and scalability, ensuring smooth operation even under high user
+            loads.
+          </li>
+          <li>
+            Type-Safe Codebase: Ensured robust, error-free development through
+            TypeScript, promoting high-quality, maintainable code.
+          </li>
+          <li>
+            Tailored UI/UX: Crafted a modern, responsive interface with Tailwind
+            CSS for an exceptional user experience across devices.
+          </li>
+        </ul>
+      </section>
+
+      <section className="mb-20 w-full">
+        <h2 className="text-4xl mb-10 text-blue-400">Tech Stack</h2>
+
+        <ul className="text-xl list-disc">
+          <li>
+            Next.js: A flexible framework enabling server-side rendering and
+            seamless user interactions.
+          </li>
+          <li>
+            TypeScript: Type-safe JavaScript for cleaner, more reliable code.
+          </li>
+          <li>
+            Tailwind CSS: Utility-first CSS framework to rapidly design
+            responsive, custom UIs.
+          </li>
+          <li>
+            OpenAI API: Harnessing advanced AI to enable dynamic, conversational
+            experiences.
+          </li>
+        </ul>
+      </section>
 
       <Chatbot />
     </main>
