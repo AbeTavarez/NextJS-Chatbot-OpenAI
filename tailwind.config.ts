42c5ffd0d65da95c75fca447549a0cfdd6ed220d
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-09-13 22:50:30 -0400

    favicon icon

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
